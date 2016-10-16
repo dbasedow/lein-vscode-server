95239a85434e3ba68072c8023afda8552efdd457
danielbasedow <daniel.basedow@gmail.com>
2016-10-16 18:19:55 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject vscode-server-lein "0.1.0-SNAPSHOT"
+  :description "Visual Studio Code Clojure support"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[jline "2.12"]
+                 [cljfmt "0.5.3"]
+                 [org.clojure/core.async "0.2.385"]
+                 [com.taoensso/timbre "4.7.4"]
+                 [cheshire "5.6.1"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/vscode_server_lein.clj
@@ -0,0 +1,6 @@
+(ns leiningen.vscode-server-lein)
+
+(defn vscode-server-lein
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
