e10133e56026a9196ba7f83a0a75047a26ec67ee
danielbasedow <daniel.basedow@gmail.com>
2016-10-30 12:05:40 +0100

    fix formatting

--- src/clj/vscodeclj/methods.clj
+++ src/clj/vscodeclj/methods.clj
@@ -30,13 +30,13 @@
     nil)
 
 (defn document-changed-method [msg]
-    (let [uri (get-in msg [:params :textDocument :uri])
-          content (get-in msg [:params :contentChanges 0 :text])]
+    (let [uri (get-in msg [:textDocument :uri])
+          content (get-in msg [:contentChanges 0 :text])]
         (swap! documents assoc uri content))
     nil)
 
 (defn document-format [msg]
-    (let [uri (get-in msg [:params :textDocument :uri])
+    (let [uri (get-in msg [:textDocument :uri])
           content (get @documents uri)
           formatted (fmt/reformat-string content)
           changes (diff/diff content formatted)]
